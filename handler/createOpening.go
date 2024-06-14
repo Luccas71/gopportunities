1669766a12fa6c467e7f40f91b21e7907b4da950
Lucas Volpato <lmaciel916@gmail.com>
2024-06-14 16:03:48 -0300

    feat: add files separate routes handlers

---
+++ handler/createOpening.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func CreateOpeningHanlder(ctx *gin.Context) {
+
+}
--- handler/handlers.go
+++ handler/handlers.go
@@ -1,19 +1,1 @@
 package handler
-
-import "github.com/gin-gonic/gin"
-
-func ShowOpeningHanlder(ctx *gin.Context) {
-
-}
-func ListOpeningsHanlder(ctx *gin.Context) {
-
-}
-func CreateOpeningHanlder(ctx *gin.Context) {
-
-}
-func UpdateOpeningHanlder(ctx *gin.Context) {
-
-}
-func DeleteOpeningHanlder(ctx *gin.Context) {
-
-}
