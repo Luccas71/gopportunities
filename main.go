e9b1be2323748c7eab92eb3aa0d2eab73ad44922
Lucas Volpato <lmaciel916@gmail.com>
2024-06-14 19:29:34 -0300

    feat: initial config

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/Luccas71/gopportunities.git/config"
 	"github.com/Luccas71/gopportunities.git/router"
 )
 
 func main() {
+	//initialize configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// initialize router
 	router.Initialize()
 }
