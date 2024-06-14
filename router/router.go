92d32bde75d72774b86bf24182ec3eb037e2509d
Lucas Volpato <lmaciel916@gmail.com>
2024-06-14 15:39:08 -0300

    feat: add router group

--- router/router.go
+++ router/router.go
@@ -5,12 +5,8 @@ import "github.com/gin-gonic/gin"
 func Initialize() {
 	//inicializa o router com a config padrão do gin
 	router := gin.Default()
-	// handler
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// inicializa as rotas
+	initializeRoutes(router)
 	// roda o servidor
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
